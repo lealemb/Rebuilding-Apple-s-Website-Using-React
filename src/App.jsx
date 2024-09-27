91809eba1ec739be8aa3017003e79e5c55e6c58a
lealemb <lealemb84@gmail.com>
2024-09-27 16:45:14 +0300

    secondsection updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './components/Header/Header.css';
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Firstsection from './components/Firstsection/Firstsection';
+import Secondsection from './components/Secondsection/Secondsection';
 
 function App() {
   
   return (
     <>
       <Header/>
+      <Firstsection/>
+      <Secondsection/>
+      <Footer/> 
     </>
   )
 }
